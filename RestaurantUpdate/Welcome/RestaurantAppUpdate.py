e18047d90828bdf3ffc14076c9aca4de70faf6f3
Jona Papa <141036620+JonaPapa@users.noreply.github.com>
2023-12-21 20:57:19 +0100

    Add files via upload

---
+++ RestaurantUpdate/Welcome/RestaurantAppUpdate.py
@@ -0,0 +1,11 @@
+#STEP 1 -Creating the App's login page.
+
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+class RestaurantAppUpdate(MDApp):
+    def build(self):
+        return
+    
+RestaurantAppUpdate().run()
