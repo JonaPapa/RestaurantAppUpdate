9b23d34272915b033b985fe9f169fb4f426b1d36
Jona Papa <141036620+JonaPapa@users.noreply.github.com>
2023-12-29 18:11:10 +0100

    Update RestaurantAppUpdate.py

--- RestaurantUpdate/Welcome/RestaurantAppUpdate.py
+++ RestaurantUpdate/Welcome/RestaurantAppUpdate.py
@@ -2,10 +2,24 @@
 
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+# Define the main screen
+class Login(Screen):
+    pass
+
+# Define the secondary screen
+class HomeScreen(Screen):
+    pass
+
+# Screen manager
+class ScreenManagement(ScreenManager):
+    pass
+
+# Main app class
 class RestaurantAppUpdate(MDApp):
     def build(self):
-        return
-    
+        return Builder.load_file('RestaurantAppUpdate.kv')
+
+#if __name__ == "__main__": #It's a way for the script to "ask itself" if it is the main module being executed or if it's being used by another script as a module.
 RestaurantAppUpdate().run()
